2616b3de507763aed966fd5aa6320eb93f6dd98a
eldaniell01 <amontepequem@miumg.edu.gt>
2023-04-30 20:38:00 -0600

    agregar fetch

--- ejercicio_1/src/App.js
+++ ejercicio_1/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+
 
 function App() {
+  
+  const [datos, setDatos] = useState({});
+
+  useEffect(() => {
+    async function fetchDatos() {
+      const respuesta = await fetch("http://localhost:8000/movies/1");
+      const datosJSON = await respuesta.json();
+      setDatos(datosJSON);
+    }
+    fetchDatos();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <li >{datos.title}</li>
       </header>
     </div>
   );
